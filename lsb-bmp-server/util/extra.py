ca59979c2bdb6177d9c4e84dd815b144ae8745cd
QinYL <qinyuelai128@yeah.net>
2023-06-29 23:37:46 +0800

    feat(extra): 裁剪部分工具类完毕

--- lsb-bmp-server/util/extra.py
+++ lsb-bmp-server/util/extra.py
@@ -7,46 +7,48 @@ uploads_dir = os.path.dirname(os.path.dirname(now_dir))
 UPLOAD_FOLDER = os.path.join(uploads_dir, 'uploads')
 
 
-def test_crop_resilience(image_path, direction, pixel_count, step):
+def test_crop_resilience(image_path, direction, cut_pixel_count, step=1):
     """
     Crop an image in all possible ways along both x and y directions and check if the message can still be extracted.
 
     Args:
         image_path (str): The path of the image to crop.
         direction (str): The direction to crop ('x', 'y', 'xy').
-        pixel_count (int): The number of pixels to crop.
-        step (int): The step size for the cropping area.
+        cut_pixel_count (int): The number of pixels to crop.
+        step (int): The step size for the cropping area. Defaults to 1.
     """
     # Load the image
     image = Image.open(image_path)
     width, height = image.size
 
     # Determine the direction to crop
-    if direction.lower() in ['x', 'xy']:
-        start_x = 0
-        end_x = width - pixel_count
-    if direction.lower() in ['y', 'xy']:
-        start_y = 0
-        end_y = height - pixel_count
+    if direction in ['x', 'xy']:
+        start_x0 = 0
+        end_x0 = cut_pixel_count + 1
+        remaining_width = width - cut_pixel_count
+    if direction in ['y', 'xy']:
+        start_y0 = 0
+        end_y0 = cut_pixel_count + 1
+        remaining_height = height - cut_pixel_count
 
     # Crop the image in all possible ways and test if the message can still be extracted
-    if direction.lower() in ['x', 'xy']:
-        for i in range(start_x, end_x, step):
-            cropped_image_path = image_path[:-4] + f"_cropped_x_{i}_{i + pixel_count}.bmp"
-            crop_image(image_path, cropped_image_path, i, 0, pixel_count, height)
+    if direction == 'x':
+        for i in range(start_x0, end_x0, step):
+            cropped_image_path = image_path[:-4] + f"_cropped_at_x_({i}_0_{remaining_width}_{height}).bmp"
+            crop_image(image_path, cropped_image_path, i, 0, remaining_width, height)
             try_to_extract_message(cropped_image_path)
 
-    if direction.lower() in ['y', 'xy']:
-        for i in range(start_y, end_y, step):
-            cropped_image_path = image_path[:-4] + f"_cropped_y_{i}_{i + pixel_count}.bmp"
-            crop_image(image_path, cropped_image_path, 0, i, width, pixel_count)
+    if direction == 'y':
+        for i in range(start_y0, end_y0, step):
+            cropped_image_path = image_path[:-4] + f"_cropped_at_y_(0_{i}_{width}_{remaining_height}).bmp"
+            crop_image(image_path, cropped_image_path, 0, i, width, remaining_height)
             try_to_extract_message(cropped_image_path)
 
-    if direction.lower() == 'xy':
-        for i in range(start_x, end_x, step):
-            for j in range(start_y, end_y, step):
-                cropped_image_path = image_path[:-4] + f"_cropped_xy_{i}_{i + pixel_count}_{j}_{j + pixel_count}.bmp"
-                crop_image(image_path, cropped_image_path, i, j, pixel_count, pixel_count)
+    if direction == 'xy':
+        for i in range(start_x0, end_x0, step):
+            for j in range(start_y0, end_y0, step):
+                cropped_image_path = image_path[:-4] + f"_cropped_at_xy_({i}_{j}_{remaining_width}_{remaining_height}).bmp"
+                crop_image(image_path, cropped_image_path, i, j, remaining_width, remaining_height)
                 try_to_extract_message(cropped_image_path)
 
 
@@ -65,15 +67,23 @@ def try_to_extract_message(cropped_image_path):
     # Use your existing function to extract the message
     try:
         message = come_on(cropped_image_path)
-        print(f"Message extracted from {cropped_image_path}: {message}")
+        if message != "Not Encrypt!":
+            print(f"Message extracted from {cropped_image_path}: {message}")
+        else:
+            # 删除不加密的图片
+            os.remove(cropped_image_path)
+            pass
     except Exception as e:
         print(f"Failed to extract message from {cropped_image_path}: {e}")
+        # 删除无法提取信息的图片
+        os.remove(cropped_image_path)
 
 
 if __name__ == '__main__':
-    test_pic = os.path.join(UPLOAD_FOLDER, '4X4_lsb_embed.bmp')
+    test_pic = os.path.join(UPLOAD_FOLDER, 'output_1688008642.bmp')
     try_to_extract_message(test_pic)
-    test_crop_resilience(test_pic, 'x', 1, 1)
-    test_crop_resilience(test_pic, 'y', 1, 1)
-    test_crop_resilience(test_pic, 'xy', 1, 1)
+    test_crop_resilience(test_pic, 'x', 3)
+    test_crop_resilience(test_pic, 'y', 3)
+    test_crop_resilience(test_pic, 'xy', 3)
+
 
